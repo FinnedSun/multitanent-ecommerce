46c6429d69842539c008d3ea2980d80e8e6eb614
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-04-15 21:21:38 +0700

    02: Customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 
 export default function Home() {
   return (
-    <div>
-      <Button>
-        Click me
-      </Button>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"eleveted"}>
+            I am a Button
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="I am a Input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea value={"I am a Textarea"} />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
