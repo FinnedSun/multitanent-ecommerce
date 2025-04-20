879ad0a81c1faa77a8e9f389b6b19402af408f93
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-04-20 20:08:43 +0700

    07: setup trpc

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,11 +1,17 @@
+"use client"
 
-
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const trpc = useTRPC()
+
+  const categories = useQuery(trpc.categories.getMany.queryOptions())
 
   return (
-    <div >
-      Home page
+    <div>
+      <p>is Loading: {`${categories.isLoading}`}</p>
+      {JSON.stringify(categories.data, null, 2)}
     </div>
   );
 }
