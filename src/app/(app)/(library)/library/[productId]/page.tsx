8a3185a522274c114d5ad6b70ea92bf65295f789
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-04-29 01:12:23 +0700

    21: Reviews

---
+++ src/app/(app)/(library)/library/[productId]/page.tsx
@@ -0,0 +1,35 @@
+import { ProductView } from '@/modules/library/ui/views/product-view';
+import { getQueryClient, trpc } from '@/trpc/server';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+export const dynamic = "force-dynamic"
+
+interface ProductIdPageProps {
+  params: Promise<{
+    productId: string
+  }>
+}
+
+const ProductIdPage = async ({
+  params
+}: ProductIdPageProps) => {
+  const { productId } = await params;
+
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.library.getOne.queryOptions({
+    productId
+  }))
+
+  void queryClient.prefetchQuery(trpc.reviews.getOne.queryOptions({
+    productId
+  }))
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductView productId={productId} />
+    </HydrationBoundary>
+  )
+}
+
+export default ProductIdPage;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,21 +1,22 @@
 // storage-adapter-import-placeholder
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
 import sharp from 'sharp'
+import { fileURLToPath } from 'url'
+import { buildConfig } from 'payload'
+import { Config } from './payload-types'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+import { multiTenantPlugin } from '@payloadcms/plugin-multi-tenant'
 
+import { Tags } from './collections/Tags'
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
-import { Categories } from './collections/Categories'
-import { multiTenantPlugin } from '@payloadcms/plugin-multi-tenant'
-import { Products } from './collections/Products'
-import { Tags } from './collections/Tags'
-import { Tenants } from './collections/Tenants'
-import { Config } from './payload-types'
 import { Orders } from './collections/Orders'
+import { Tenants } from './collections/Tenants'
+import { Reviews } from './collections/Reviews'
+import { Products } from './collections/Products'
+import { Categories } from './collections/Categories'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -27,7 +28,16 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, Categories, Products, Tags, Tenants, Orders],
+  collections: [
+    Users,
+    Media,
+    Categories,
+    Products,
+    Tags,
+    Tenants,
+    Orders,
+    Reviews,
+  ],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
