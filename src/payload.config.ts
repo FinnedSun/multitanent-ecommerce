f8c9dbd09fe86ba003d268bcb524772ccc98dc48
FinnedSun <rayhanagungmaulana.04@gmail.com>
2025-05-03 21:19:07 +0700

    29: Storage Adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
 import { buildConfig } from 'payload'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { Config } from './payload-types'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
@@ -63,6 +64,12 @@ export default buildConfig({
       },
       userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 })
